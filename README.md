3725e6266de712fc02f350cb31fe50a14ffd602f
Kota Mizushima <mizukota@gmail.com>
2023-06-15 13:22:30 +0900

    ファイル読み込み「関数」のサンプル

---
+++ README.md
@@ -0,0 +1,18 @@
+# chatgpt-api-functions
+
+ChatGPT APIで「関数呼び出し」機能を使うサンプルです
+
+## インストール
+
+```console
+python -m venv venv # 仮想Python環境作成
+source venv/bin/activate # アクティベート
+pip install -r requirements.txt # 依存ライブラリインストール
+```
+
+## 実行
+
+```
+export OPENAI_API_KEY=<your api key>
+python main.py
+```
---
+++ main.py
@@ -0,0 +1,71 @@
+import openai
+import json
+import json
+
+# ファイルからテキストを読み込む関数
+def read_local_file(path):
+    """Get the file content in a given path"""
+    with open(path) as f:
+        content = f.read()
+        file_info = {
+            "content": content
+        }
+        return json.dumps(file_info)
+
+# Step 1, send model the user query and what functions it has access to
+def run_conversation():
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=[{"role": "user", "content": "main.pyの内容を表示してください"}],
+        functions=[
+            {
+                "name": "read_local_file",
+                "description": "Get the file content in a given path",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "path": {
+                            "type": "string",
+                            "description": "The path of file",
+                        },
+                    },
+                    "required": ["path"],
+                },
+            }
+        ],
+        function_call="auto",
+    )
+
+    message = response["choices"][0]["message"]
+
+    # Step 2, check if the model wants to call a function
+    if message.get("function_call"):
+        function_name = message["function_call"]["name"]
+
+        arguments_json = message["function_call"]["arguments"]
+
+        path = json.loads(arguments_json)["path"]
+
+        # Step 3, call the function
+        # Note: the JSON response from the model may not be valid JSON
+        function_response = read_local_file(
+            path=path
+        )
+
+        # Step 4, send model the info on the function call and function response
+        second_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=[
+                {"role": "user", "content": "main.pyの内容を表示してください"},
+                message,
+                {
+                    "role": "function",
+                    "name": function_name,
+                    "content": function_response,
+                },
+            ],
+        )
+        return second_response
+
+response = run_conversation()
+print(response["choices"][0]["message"]["content"])
